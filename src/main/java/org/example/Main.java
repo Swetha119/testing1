f9f7aff166f710c8b8b8595f7dc949a4c3e23536
admin1124 <88696086+admin1124@users.noreply.github.com>
2025-02-15 07:37:13 +0530

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main{
         Book book = new Book("Author", "Title of the Book ");
         book.displayDetails();
 
+
         // Creating an AudioBook object
         AudioBook audiobook = new AudioBook("Auther", "Title of the Book", 1.2);
         audiobook.displayDetails();
